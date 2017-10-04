e08fffeb19a6b38b43b3d4bb7f3285e69df83838
Gary Hetzel <garyhetzel@gmail.com>
2017-10-03 22:34:02 -0400

    Got it working

--- cando.c
+++ cando.c
@@ -7,303 +7,233 @@
  */
 
 #include <linux/module.h>
-#include <linux/init.h>
-#include <linux/slab.h>         /* kmalloc() */
-#include <linux/usb.h>          /* USB stuff */
-#include <linux/mutex.h>        /* mutexes */
-#include <linux/ioctl.h>
-
-#define CANDO_USB_VENDOR_ID  0x2087
-#define CANDO_USB_PRODUCT_ID 0x0a01
-
-struct usb_cando {
-    struct usb_device              *udev;
-    struct usb_interface           *interface;
-    unsigned char                  minor;
-    char                           serial_number[8];
-    int                            open_count;       /* Open count for this port */
-    struct semaphore               sem;              /* Locks this structure */
-    spinlock_t                     cmd_spinlock;     /* locks dev->command */
-    char                           *int_in_buffer;
-    struct usb_endpoint_descriptor *int_in_endpoint;
-    struct urb                     *int_in_urb;
-    int                            int_in_running;
-    // char                           *ctrl_buffer;
-    // struct urb                     *ctrl_urb;
-    // struct usb_ctrlrequest         *ctrl_dr;
-};
-
-static DEFINE_MUTEX(disconnect_mutex);
-static struct usb_driver cando_driver;
-
-// -----------------------------------------------------------------------------
-static inline void cando_debug_data(const char *function, int size,
-        const unsigned char *data)
-{
-    int i;
-    printk(KERN_INFO "[debug] %s: length = %d, data = \n", function, size);
-
-    for (i = 0; i < size; ++i) {
-        printk(KERN_INFO "%.2x \n", data[i]);
-    }
-
-    printk("\n");
-}
-
-static void cando_abort_transfers(struct usb_cando *dev)
-{
-    if (!dev) {
-        printk(KERN_CRIT "dev is NULL\n");
-        return;
-    }
-
-    if (!dev->udev) {
-        printk(KERN_CRIT "udev is NULL\n");
-        return;
-    }
-
-    if (dev->udev->state == USB_STATE_NOTATTACHED) {
-        printk(KERN_CRIT "udev not attached\n");
-        return;
-    }
-
-    /* Shutdown transfer */
-    if (dev->int_in_running) {
-        dev->int_in_running = 0;
-        mb();
-
-        if (dev->int_in_urb) {
-            usb_kill_urb(dev->int_in_urb);
-        }
-    }
-}
-
-static inline void cando_delete(struct usb_cando *dev)
-{
-    cando_abort_transfers(dev);
-
-    /* Free data structures. */
-    if (dev->int_in_urb) {
-        usb_free_urb(dev->int_in_urb);
-    }
-
-    kfree(dev->int_in_buffer);
-    kfree(dev);
-}
-
-// -----------------------------------------------------------------------------
-static struct usb_device_id cando_table [] = {
-    { USB_DEVICE(CANDO_USB_VENDOR_ID, CANDO_USB_PRODUCT_ID) },
-    { }
-};
-MODULE_DEVICE_TABLE(usb,cando_table);
-
-// -----------------------------------------------------------------------------
-static void cando_int_in_callback(struct urb *urb)
-{
-    struct usb_cando *dev = urb->context;
-    int retval;
-
-    printk(KERN_INFO "cando: INT\n");
-
-    cando_debug_data(__FUNCTION__, urb->actual_length, urb->transfer_buffer);
-
-    if (urb->status) {
-        if (
-            urb->status == -ENOENT     ||
-            urb->status == -ECONNRESET ||
-            urb->status == -ESHUTDOWN
-        ) {
-            return;
-        } else {
-            printk(KERN_CRIT "non-zero urb status (%d)", urb->status);
-            goto resubmit; /* Maybe we can recover. */
-        }
-    }
-
-    if (urb->actual_length > 0) {
-        spin_lock(&dev->cmd_spinlock);
-
-        // is this the data we want?
-        // dev->int_in_buffer
-
-        spin_unlock(&dev->cmd_spinlock);
-    }
-
-resubmit:
-    /* Resubmit if we're still running. */
-    if (dev->int_in_running && dev->udev) {
-        retval = usb_submit_urb(dev->int_in_urb, GFP_ATOMIC);
-        if (retval) {
-            printk(KERN_CRIT "resubmitting urb failed (%d)", retval);
-            dev->int_in_running = 0;
-        }
-    }
-}
-
-// static struct file_operations cando_fops = {
-//     .owner   = THIS_MODULE,
-//     .open    = cando_open,
-//     .release = cando_release,
-// };
-
-// static struct usb_class_driver cando_class = {
-//     .name       = "cando%d",
-//     .fops       = &cando_fops,
-//     .minor_base = CANDO_MINOR_BASE,
-// };
-
-// -----------------------------------------------------------------------------
-static int cando_probe(struct usb_interface *interface,
-    const struct usb_device_id *id)
-{
-    struct usb_device *udev = interface_to_usbdev(interface);
-    struct usb_cando *dev = NULL;
-    struct usb_host_interface *iface_desc;
-    struct usb_endpoint_descriptor *endpoint;
-    int i, int_end_size;
-    int retval = -ENODEV;
-
-    printk(KERN_INFO "Starting Cando probe vendor=%04x product=%04x\n",
-        id->idVendor,
-        id->idProduct);
-
-    if (!udev) {
-        printk(KERN_CRIT "udev is NULL\n");
-        goto exit;
-    }
-
-    dev = kzalloc(sizeof(struct usb_cando), GFP_KERNEL);
-
-    if (!dev) {
-        printk(KERN_CRIT "cannot allocate memory for struct usb_cando\n");
-        retval = -ENOMEM;
-        goto exit;
-    }
-
-    sema_init(&dev->sem, 1);
-    spin_lock_init(&dev->cmd_spinlock);
-
-    dev->udev = udev;
-    dev->interface = interface;
-    iface_desc = interface->cur_altsetting;
-
-    /* Set up interrupt endpoint information. */
-    for (i = 0; i < iface_desc->desc.bNumEndpoints; ++i) {
-        endpoint = &iface_desc->endpoint[i].desc;
-
-        if (
-            ((endpoint->bEndpointAddress & USB_ENDPOINT_DIR_MASK) == USB_DIR_IN) &&
-            ((endpoint->bmAttributes & USB_ENDPOINT_XFERTYPE_MASK) == USB_ENDPOINT_XFER_INT)
-        ) {
-            dev->int_in_endpoint = endpoint;
-        }
-
-    }
-
-    if (!dev->int_in_endpoint) {
-        printk(KERN_CRIT "could not find interrupt in endpoint\n");
-        goto error;
-    }
-
-    int_end_size = le16_to_cpu(dev->int_in_endpoint->wMaxPacketSize);
-
-    dev->int_in_buffer = kmalloc(int_end_size, GFP_KERNEL);
-
-    if (!dev->int_in_buffer) {
-        printk(KERN_CRIT "could not allocate int_in_buffer\n");
-        retval = -ENOMEM;
-        goto error;
-    }
-
-    dev->int_in_urb = usb_alloc_urb(0, GFP_KERNEL);
-    if (!dev->int_in_urb) {
-        printk(KERN_CRIT "could not allocate int_in_urb\n");
-        retval = -ENOMEM;
-        goto error;
-    }
-
-    // Retrieve a serial.
-    if (!usb_string(udev, udev->descriptor.iSerialNumber,
-        dev->serial_number, sizeof(dev->serial_number))) {
-        printk(KERN_CRIT "could not retrieve serial number\n");
-        goto error;
-    }
-
-    // Save our data pointer in this interface device
-    usb_set_intfdata(interface, dev);
-
-    /* We can register the device now, as it is ready. */
-    // retval = usb_register_dev(interface, &cando_class);
-    printk(KERN_INFO "[%d] cando int setup: endpoint=%x\n",
-        udev->descriptor.iSerialNumber,
-        dev->int_in_endpoint->bEndpointAddress);
-
-    // Initialize interrupt URB.
-    usb_fill_int_urb(
-        dev->int_in_urb,
-        dev->udev,
-        usb_rcvintpipe(
-            dev->udev,
-            dev->int_in_endpoint->bEndpointAddress
-        ),
-        dev->int_in_buffer,
-        le16_to_cpu(dev->int_in_endpoint->wMaxPacketSize),
-        cando_int_in_callback,
-        dev,
-        dev->int_in_endpoint->bInterval
-    );
-
-    printk(KERN_INFO "Cando Touchscreen driver loaded successfully\n");
-
-exit:
-    return retval;
-
-error:
-    cando_delete(dev);
-    return retval;
-}
-
-// -----------------------------------------------------------------------------
-static void cando_disconnect(struct usb_interface *interface)
-{
-    /* called when unplugging a USB device. */
-}
-
-static struct usb_driver cando_driver = {
-    .name       = "cando",
-    .id_table   = cando_table,
-    .probe      = cando_probe,
-    .disconnect = cando_disconnect,
-};
-
-// -----------------------------------------------------------------------------
-static int __init usb_cando_init(void)
-{
-    int result;
-
-    printk(KERN_INFO "Register Cando touchscreen driver\n");
-    result = usb_register(&cando_driver);
-
-    if (result) {
-        printk(KERN_CRIT "registering Cando touchscreen driver failed\n");
-    } else {
-        printk(KERN_INFO "Cando touchscreen driver registered successfully\n");
-    }
-
-    return result;
-}
-
-// -----------------------------------------------------------------------------
-static void __exit usb_cando_exit(void)
-{
-    usb_deregister(&cando_driver);
-    printk(KERN_INFO "Cando touchscreen module deregistered\n");
-}
-
-module_init(usb_cando_init);
-module_exit(usb_cando_exit);
+#include <linux/device.h>
+#include <linux/hid.h>
+#include <linux/input.h>
+#include <linux/input/mt.h>
 
 MODULE_AUTHOR("Gary Hetzel <garyhetzel@gmail.com>");
 MODULE_DESCRIPTION("Cando USB Touchscreen driver");
 MODULE_LICENSE("GPL");
+
+#define CANDO_USB_VENDOR_ID  0x2087
+#define CANDO_USB_PRODUCT_ID 0x0a01
+#define CANDO_ABS_X_MAX      4095
+#define CANDO_ABS_Y_MAX      4095
+
+
+struct coords {
+    int x;
+    int y;
+    int state;
+};
+
+struct cando_device {
+    struct hid_device              *hdev;
+    const struct hid_device_id     *id;
+    struct input_dev               *input;
+    struct coords                  *last1;
+    struct coords                  *last2;
+};
+
+// -----------------------------------------------------------------------------
+static void cando_send_touch_event(struct input_dev *input, int contact_num,
+    int x, int y, int state, struct coords *last)
+{
+    if (state) {
+        // send multitouch events
+        input_event(input, EV_ABS, ABS_MT_SLOT, contact_num);
+        input_report_abs(input, ABS_MT_TRACKING_ID, contact_num);
+        input_report_abs(input, ABS_MT_POSITION_X, x);
+        input_report_abs(input, ABS_MT_POSITION_Y, y);
+
+        // send touch event
+        input_report_key(input, BTN_TOUCH, 1);
+
+        // send traditional single-touch (ST) events
+        input_report_abs(input, ABS_X, x);
+        input_report_abs(input, ABS_Y, y);
+
+        input_event(input, EV_SYN, SYN_REPORT, 0);
+        input_sync(input);
+
+        last->x = x;
+        last->y = y;
+        last->state = 1;
+    } else {
+        input_event(input, EV_ABS, ABS_MT_SLOT, contact_num);
+        input_report_abs(input, ABS_MT_TRACKING_ID, -1);
+        input_report_key(input, BTN_TOUCH, 0);
+        input_event(input, EV_SYN, SYN_REPORT, 0);
+        input_sync(input);
+
+        last->state = 0;
+    }
+}
+
+// -----------------------------------------------------------------------------
+static void cando_report(struct hid_device *hdev, struct hid_report *report)
+{
+    struct cando_device *cdev = hid_get_drvdata(hdev);
+    int i;
+    unsigned count;
+    struct hid_field *field;
+    int32_t value;
+    int32_t active1, active2, x1, y1, x2, y2, active_contacts;
+
+    if(!cdev || !cdev->input) {
+        return;
+    }
+
+    // iterate through all fields in this report
+    for (i = 0; i < report->maxfield; i++) {
+        field = report->field[i];
+        count = field->report_count;
+
+        if (!(HID_MAIN_ITEM_VARIABLE & field->flags))
+            continue;
+
+        value = (int32_t)(*field->value);
+
+        switch(i) {
+        case 0: // Contact 1 touch detect
+            if (value) {
+                active1 = 1;
+            } else {
+                active1 = 0;
+            }
+            break;
+
+        case 3: // Contact 1 X-coordinate
+            x1 = value;
+            break;
+
+        case 4: // Contact 1 Y-coordinate
+            y1 = value;
+            break;
+
+        case 5: // Contact 2 touch detect
+            if (value) {
+                active2 = 1;
+            } else {
+                active2 = 0;
+            }
+            break;
+
+        case 8: // Contact 2 X-coordinate
+            x2 = value;
+            break;
+
+        case 9: // Contact 2 Y-coordinate
+            y2 = value;
+            break;
+
+        case 10: // Total active contact count
+            active_contacts = value;
+            break;
+        }
+    }
+
+    cando_send_touch_event(cdev->input, 0, x1, y1, active1, cdev->last1);
+    cando_send_touch_event(cdev->input, 1, x2, y2, active2, cdev->last2);
+}
+
+// -----------------------------------------------------------------------------
+static int cando_probe(struct hid_device *hdev, const struct hid_device_id *id)
+{
+    struct cando_device *cdev = NULL;
+    struct input_dev *input;
+    int ret = -ENODEV;
+
+    printk(KERN_INFO "Starting Cando probe bus=%02x group=%02x vendor=%04x product=%04x\n",
+        id->bus,
+        id->group,
+        id->vendor,
+        id->product
+    );
+
+    // allocate devices for input subsystem
+    // ----------------------------------------
+    input = input_allocate_device();
+
+    if (!input) {
+        ret = -ENOMEM;
+        printk(KERN_CRIT "Failed to allocate input: %d\n", ret);
+        return ret;
+    }
+
+    input->name        = "Cando Multitouch Driver";
+    input->id.bustype  = BUS_USB;
+    input->dev.parent  = &hdev->dev;
+
+    input_set_capability(input, EV_KEY, BTN_TOUCH);
+    input_set_capability(input, EV_KEY, BTN_TOUCH);
+
+    input_set_abs_params(input, ABS_X, 0, CANDO_ABS_X_MAX, 0, 0);
+    input_set_abs_params(input, ABS_Y, 0, CANDO_ABS_Y_MAX, 0, 0);
+    input_mt_init_slots(input, 2, 0);
+    input_set_abs_params(input, ABS_MT_SLOT, 0, 1, 0, 0);
+    input_set_abs_params(input, ABS_MT_TRACKING_ID, 0, 65535, 0, 0);
+    input_set_abs_params(input, ABS_MT_POSITION_X, 0, CANDO_ABS_X_MAX, 0, 0);
+    input_set_abs_params(input, ABS_MT_POSITION_Y, 0, CANDO_ABS_Y_MAX, 0, 0);
+
+    // allocate driver struct
+    // ----------------------------------------
+    cdev = devm_kzalloc(&hdev->dev, sizeof(struct cando_device), GFP_KERNEL);
+
+    if (!cdev) {
+        dev_err(&hdev->dev, "cannot allocate memory for struct cando_device\n");
+        return -ENOMEM;
+    }
+
+    // populate driver-specific struct
+    // ----------------------------------------
+    cdev->hdev  = hdev;
+    cdev->id    = id;
+    cdev->input = input;
+    cdev->last1 = &(struct coords){ 0, 0, 0 };
+    cdev->last2 = &(struct coords){ 0, 0, 0 };
+
+    // final setup
+    // ----------------------------------------
+    hid_set_drvdata(hdev, cdev);
+    input_set_drvdata(input, cdev);
+
+    ret = hid_parse(hdev);
+
+    if (ret != 0) {
+        return ret;
+    }
+
+    ret = hid_hw_start(hdev, HID_CONNECT_DEFAULT);
+
+    if (ret) {
+        return ret;
+    }
+
+    printk(KERN_INFO "Cando Touchscreen driver loaded successfully\n");
+    return 0;
+}
+
+// -----------------------------------------------------------------------------
+static const struct hid_device_id cando_table[] = {
+    { HID_DEVICE(BUS_USB, HID_GROUP_MULTITOUCH, CANDO_USB_VENDOR_ID,
+        CANDO_USB_PRODUCT_ID) },
+    { }
+};
+MODULE_DEVICE_TABLE(hid, cando_table);
+
+static const struct hid_usage_id cando_grabbed_usages[] = {
+    { HID_ANY_ID, HID_ANY_ID, HID_ANY_ID },
+    { HID_ANY_ID - 1, HID_ANY_ID - 1, HID_ANY_ID - 1}
+};
+
+static struct hid_driver cando_driver = {
+    .name             = "cando",
+    .id_table         = cando_table,
+    .probe            = cando_probe,
+    // .remove           =
+    // .event            = cando_event,
+    .report           = cando_report,
+};
+module_hid_driver(cando_driver);
